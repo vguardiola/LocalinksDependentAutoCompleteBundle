4063efe8aa4597c9e97814517eaca4987cc5b523
Mathieu Hautenauve <mhautenauve@gmail.com>
2018-01-15 00:21:14 +0100

    + Base functionalities

---
+++ Form/Extension/DependentAutoCompleteTypeExtension.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Localinks\DependentAutoCompleteBundle\Form\Extension;
+
+use Sonata\AdminBundle\Form\Type\ModelAutocompleteType;
+use Symfony\Component\Form\AbstractTypeExtension;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+
+class DependentAutoCompleteTypeExtension extends AbstractTypeExtension
+{
+    /**
+     * Returns the name of the type being extended.
+     *
+     * @return string The name of the type being extended
+     */
+    public function getExtendedType()
+    {
+        return ModelAutocompleteType::class;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefined(array('dependencies'));
+
+        $resolver->setDefaults([
+            'template' => 'LocalinksDependentAutoCompleteBundle:Form:dependent_auto_complete_type.html.twig',
+            'dependencies' => array()
+        ]);
+    }
+
+    public function buildView(FormView $view, FormInterface $form, array $options)
+    {
+        if (isset($options['dependencies'])) {
+            $view->vars['dependencies'] = $options['dependencies'];
+        }
+    }
+
+}
--- LocalinksDependentAutoCompleteBundle.php
+++ LocalinksDependentAutoCompleteBundle.php
@@ -2,8 +2,10 @@
 
 namespace Localinks\DependentAutoCompleteBundle;
 
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class LocalinksDependentAutoCompleteBundle extends Bundle
 {
+    
 }
